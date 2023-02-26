7a661ce62aee012ffb9427eb3c5ec188de81b33a
Carmen <aluesc0557@ieselcaminas.org>
2023-02-26 19:53:27 +0100

    Proyecto documentado

--- src/refactorizar/Refactorizar.java
+++ src/refactorizar/Refactorizar.java
@@ -1,7 +1,19 @@
 package refactorizar;
 
+/**
+ * Clase Refactorizar:
+ * Calcula todos los numeros primos hasta el numero introducido por el usuario. Lo calcula usando la criba de Eratostenes.
+ * @author Carmen Y. Escrig
+ * @version 1.1.3
+ * @see <a href ="https://es.wikipedia.org/wiki/Criba_de_Eratóstenes">Criba de Eratostenes</a>
+ */
+
 import java.util.Scanner;
 public class Refactorizar {
+
+    /**
+     * Metodo main donde se lee el numero que desea el usuario y se muestra tanto el vector de numeros inicial, como el vector final con solo los numeros primos
+     */
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
@@ -24,6 +36,12 @@ public class Refactorizar {
             System.out.print(vector[i] + "\t");
         }
     }
+
+    /**
+     * Metodo que se encarga de generar todos los numeros primso que hay hasta el numero indicado por el usuario
+     * @param max valor entero que corresponde al numero maximo hasta los que hay que bsucar primos
+     * @return array list de valores enteros con todos los nuemros primos
+     */
     public static int[] generarPrimos (int max) {
         if (max >= 2) {
             int dim = max + 1;
@@ -37,6 +55,14 @@ public class Refactorizar {
         }
     }
 
+    /**
+     * Metodo que genera todos los numeros primos
+     * @param dim valor entero que corresponde al tamano maximo del array
+     * @param esPrimo array de booleanos con valores true o false segun si son primos o no
+     * @param cuenta valro entero que corresponde al numero de primos que hay
+     * @return array de valores enteros con todos los valores primos
+     * @since 1.1.2
+     */
     private static int[] vectorPrimos(int dim, boolean[] esPrimo, int cuenta) {
         int j;
         int i;
@@ -48,6 +74,14 @@ public class Refactorizar {
         return primos;
     }
 
+    /**
+     * Metodo que cuenta todos los numeros primos que hay
+     * @param dim valor entero que corresponde al tamano maximo del array
+     * @param esPrimo array de booleanos con los valores true o false segun si son primos o no
+     * @return valor entero que indica todos los numeros primos encontrados
+     * @since 1.1.2
+     */
+
     private static int contarPrimos(int dim, boolean[] esPrimo) {
         int cuenta = 0;
         for (int i = 0; i < dim; i++) {
@@ -57,6 +91,13 @@ public class Refactorizar {
         }
         return cuenta;
     }
+
+    /**
+     * Metodo que genera un array de booleanos con el valor true en los posibles primos
+     * @param dim valor entero que corresponde al tamano maximo del array
+     * @return array de booleanos con todos los valores a true menos el 0 y el 1 porque no pueden ser primos
+     * @since 1.1.2
+     */
 
     public static boolean[] generarArray (int dim) {
         boolean[] esPrimo = new boolean[dim];
@@ -68,6 +109,17 @@ public class Refactorizar {
 
         return esPrimo;
     }
+
+    /**
+     * Metodo que pone a false los numeros que son multiplos, y por lo tanto no pueden ser primos
+     * @param esPrimo array de booleanos que indican los posibles primos
+     * @param dim valor entero que corresponde al tamano maximo del array
+     * @return array de booleanos con los valores: <ul>
+     *      *     <li>true si es posible que sea un primo</li>
+     *      *     <li>false si no pueden ser primos</li>
+     *      * </ul>
+     * @since 1.1.2
+     */
 
     public static boolean[] quitarMultiplos (boolean[] esPrimo, int dim) {
         for (int i = 2; i < Math.sqrt(dim)+1; i++) {
